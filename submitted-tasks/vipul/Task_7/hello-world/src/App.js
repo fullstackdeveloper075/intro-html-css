4ecd84dce46ca5a6bc90c729ae5f1d3a3bc8a358
vipulvyas <vipul.vyas@smartserv.io>
2021-04-05 17:34:58 +0530

    Task: 7,8

---
+++ submitted-tasks/vipul/Task_7/hello-world/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/hello-world'
+import HelloWorld from './components/hello-world';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <HelloWorld/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ submitted-tasks/vipul/Task_8/signup/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import SignUp from './components/signup';
+class App extends Component {
+  render() {
+    return (
+      <SignUp />
+    );
+  }
+}
+
+export default App;
